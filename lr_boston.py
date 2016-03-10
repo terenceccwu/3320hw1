64e928fc8821077a68227be99eb6f938ecaf4852
Terence Wu <terencecccwu@gmail.com>
2016-03-11 01:20:53 +0800

    done boston

--- lr_boston.py
+++ lr_boston.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
@@ -6,27 +7,36 @@ import seaborn; seaborn.set()
 # Load the diabetes dataset
 boston = datasets.load_boston()
 
-# which feature
-i_feature = 0
-# Get the feature name
-feature_name = boston.feature_names[i_feature]
-
-# Use only one feature
-diabetes_X = boston.data[:, np.newaxis, i_feature]
-
-# Split the data into training/testing sets
-boston_X_train = diabetes_X[:-20]
-boston_X_test = diabetes_X[-20:]
-
 # Split the targets into training/testing sets
 boston_y_train = boston.target[:-20]
 boston_y_test = boston.target[-20:]
 
-# Create linear regression object
-model = linear_model.LinearRegression()
 
-# Train the model using the training sets
-model.fit(boston_X_train, boston_y_train)
+result = []
+# which feature
+for i_feature in range(0,13):
+	# Use only one feature
+	diabetes_X = boston.data[:, np.newaxis, i_feature]
 
-# Explained variance score: score=1 is perfect prediction
-model_score = model.score(boston_X_test, boston_y_test)
+	# Split the data into training/testing sets
+	boston_X_train = diabetes_X[:-20]
+	boston_X_test = diabetes_X[-20:]
+
+	# Create linear regression object
+	model = linear_model.LinearRegression()
+
+	# Train the model using the training sets
+	model.fit(boston_X_train, boston_y_train)
+	print "y = %dx + %d" %(model.coef_, model.intercept_)
+	# Explained variance score: score=1 is perfect prediction
+	score = model.score(boston_X_test, boston_y_test)
+	
+	result.append(score)
+
+maxindex =  np.argmax(result)
+print "Best fitted feature name is: %s" % boston.feature_names[maxindex]
+print "Best fitted model score is: %f" % result[maxindex]
+
+
+plt.plot(boston.data[:, np.newaxis, maxindex][-20:], boston_y_test, 'bo')
+plt.show()
