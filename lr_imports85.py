40c6f0f17ad87cedf2db07b7c56c10c47097b5f5
Terence Wu <terencecccwu@gmail.com>
2016-03-12 14:48:14 +0800

    finished 3.2.4 and before

--- lr_imports85.py
+++ lr_imports85.py
@@ -5,9 +5,32 @@ from sklearn import datasets, linear_model
 import seaborn
 seaborn.set()
 
-# df = pd.read_csv('imports-85.data',
-#                  header=None,
-#                  names=[#Give exact names to columns according to imports-85.names#],
-#                      na_values=(#Some values are missing, treat the '?' with NaN#))
+# 3.2.1
+names = ['symboling', 'normalized-losses','make','fuel-type','aspiration','num-of-doors','body-style','drive-wheels','engine-location','wheel-base','length','width','height','curb-weight','engine-type','num-of-cylinders','engine-size','fuel-system','bore','stroke','compression-ratio','horsepower','peak-rpm','city-mpg','highway-mpg','price']
 
+df = pd.read_csv('imports-85.data',
+                 header=None,
+                 names=names,
+                     na_values=('?'))
 
+# 3.2.2
+dropdf = df.dropna()
+
+# 3.2.3
+slice = int(round(len(dropdf)*0.8))
+train_x = dropdf[:slice]['engine-size'].values.reshape(-1,1)
+train_y = dropdf[:slice]['price'].values.reshape(-1,1)
+test_x = dropdf[slice:]['engine-size'].values.reshape(-1,1)
+test_y = dropdf[slice:]['price'].values.reshape(-1,1)
+
+# 3.2.4
+model = linear_model.LinearRegression()
+model.fit(train_x,train_y)
+
+plt.scatter(test_x, test_y, c='b')
+plt.plot(test_x, model.predict(test_x), 'r-')
+plt.xlabel("Engine size")
+plt.ylabel("Price")
+plt.show()
+
+print "Price prediction for engine size equals to 175 is: %.2f" % model.predict(175)
