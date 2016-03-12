ef62786c9b65611bc30e4679ea4c093d0e21545b
Terence Wu <terencecccwu@gmail.com>
2016-03-13 02:23:25 +0800

    finished part 3

--- lr_imports85.py
+++ lr_imports85.py
@@ -31,18 +31,20 @@ plt.scatter(test_x, test_y, c='b')
 plt.plot(test_x, model.predict(test_x), 'r-')
 plt.xlabel("Engine size")
 plt.ylabel("Price")
+plt.suptile("Linear regression on clean data")
 plt.show()
 
 print "Price prediction for engine size equals to 175 is: %.2f" % model.predict(175)
 
 # 3.2.4
 
-scaler = StandardScaler()
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
 
-scaled_train_x = scaler.fit_transform(train_x.astype('float'))
-scaled_train_y = scaler.fit_transform(train_y.astype('float'))
-scaled_test_x = scaler.fit_transform(test_x.astype('float'))
-scaled_test_y = scaler.fit_transform(test_y.astype('float'))
+scaled_train_x = scaler_x.fit_transform(train_x.astype('float'))
+scaled_train_y = scaler_y.fit_transform(train_y.astype('float'))
+scaled_test_x = scaler_x.transform(test_x.astype('float'))
+scaled_test_y = scaler_y.transform(test_y.astype('float'))
 
 model.fit(scaled_train_x,scaled_train_y)
 
@@ -50,4 +52,5 @@ plt.scatter(scaled_test_x, scaled_test_y, c='b')
 plt.plot(scaled_test_x, model.predict(scaled_test_x), 'r-')
 plt.xlabel("Standard Engine size")
 plt.ylabel("Standard Price")
+plt.suptile("Linear regression on standardized data")
 plt.show()
--- lr_regular.py
+++ lr_regular.py
@@ -10,6 +10,7 @@ y_train = [[7.5], [9.1], [13.2], [17.5], [19.3]]
 X_test = [[6], [8], [11], [16]]
 y_test = [[8.3], [12.5], [15.4], [18.6]]
 
+# 3.3.1 Linear
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 
@@ -18,15 +19,44 @@ print "Linear regression (order 1) model score is: %.3f" %lr_score
 
 xx = np.linspace(0, 26, 100)
 yy = lr_model.predict(xx.reshape(-1, 1))
-plt.plot(xx, yy)
+plt.plot(xx, yy, 'b-')
+plt.plot(X_test,y_test,'ro')
 plt.suptitle('Linear regression (order 1) result.')
+print "y = %.2f + %.2fx" %(lr_model.intercept_, lr_model.coef_)
 plt.show()
 
-# poly = PolynomialFeatures(degree=5)
-# X_train_poly = poly.fit_transform(X_train)
-# X_test_poly = poly.transform(X_test)
-#
-#
-# ridge_model = Ridge(alpha=4, normalize=False)
-# ridge_model.fit(X_train_poly, y_train)
+# 3.3.2 Poly (order 5)
+poly = PolynomialFeatures(degree=5)
+X_train_poly = poly.fit_transform(X_train)
+X_test_poly = poly.transform(X_test)
 
+lr_model.fit(X_train_poly,y_train)
+
+xx_poly = poly.transform(xx.reshape(xx.shape[0], 1))
+yy_poly = lr_model.predict(xx_poly)
+plt.plot(xx,yy_poly,'b-')
+plt.suptitle("Linear regression (order 5) result")
+
+print lr_model.coef_
+print lr_model.intercept_
+
+print "Linear regression (order 5) score is: %.3f" %lr_model.score(X_test_poly, y_test)
+
+plt.plot(X_test,y_test,'ro')
+plt.show()
+
+# 3.3.3
+
+ridge_model = Ridge(alpha=1, normalize=False)
+ridge_model.fit(X_train_poly, y_train)
+yy_ridge = ridge_model.predict(xx_poly)
+
+print ridge_model.coef_
+print ridge_model.intercept_
+
+print "Ridge regression (order 5) score is: %.3f" %ridge_model.score(X_test_poly, y_test)
+
+plt.plot(xx,yy_ridge,'b-')
+plt.plot(X_test,y_test,'ro')
+plt.suptitle("Ridge regression (order 5) result")
+plt.show()
