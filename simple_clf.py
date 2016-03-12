623f42fd7e793716b2badf3b4f2bcae5c2a1821a
Terence Wu <terencecccwu@gmail.com>
2016-03-13 03:48:47 +0800

    finished Linear Classification

--- simple_clf.py
+++ simple_clf.py
@@ -15,5 +15,25 @@ y[:n_samples // 2] = 0
 y[n_samples // 2:] = 1
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=42)
-log_reg = linear_model.LogisticRegression()
 
+# 4.1
+log_reg = linear_model.LogisticRegression()
+log_reg.fit(X_train,y_train)
+
+y_predict = log_reg.predict(X_test)
+
+for i in range(0,len(X_test)):
+	if y_predict[i]:
+		plt.scatter(X_test[i,0],X_test[i,1], c='r')
+	else:
+		plt.scatter(X_test[i,0],X_test[i,1], c='b')
+
+plt.show()
+
+# 4.2
+counter = 0
+for i in range(0,len(y_test)):
+	if y_test[i] != y_predict[i]:
+		counter += 1
+
+print "Number of wrong predictions is: %d" %counter
